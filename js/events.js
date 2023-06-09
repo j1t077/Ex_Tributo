cf632320f6644871afd1ded8227a5def96d08e25
JSanLazaro <jsanlazaro@hotmail.com>
2023-06-09 23:38:36 +0200

    all video runs

--- js/events.js
+++ js/events.js
@@ -1,5 +1,12 @@
-import { nextSlide, previousSlide } from "./videos.js";
+import { nextSlide, previousSlide, showVideo } from "./videos.js";
 const leftButton = document.getElementById("left_button");
-leftButton.addEventListener("click", previousSlide());
+leftButton.addEventListener("click", previousSlide);
 const rightButton = document.getElementById("right_button");
-leftButton.addEventListener("click", nextSlide());
+rightButton.addEventListener("click", nextSlide);
+let fotos = document.getElementsByClassName("showSlider__image");
+for (let i = 0; i < fotos.length; i++) {
+  const foto = fotos[i];
+  foto.addEventListener("click", () => {
+    showVideo(i);
+  });
+}
--- js/videos.js
+++ js/videos.js
@@ -28,12 +28,8 @@ function slidesDisplay(index) {
   slides[index + 1].style.display = "inline-block";
   slides[index + 2].style.display = "inline-block";
 }
-let fotos = document.getElementsByClassName("showSlider__image");
-for (let i = 0; i < fotos.length; i++) {
-  const foto = fotos[i];
-  foto.addEventListener("click", mostrarVideo(i));
-}
-function mostrarVideo(index) {
+
+export function showVideo(index) {
   const iframe = document.getElementById("video_content__iframe");
   iframe.setAttribute("src", videoData.result[index].videoUrl);
 
@@ -45,33 +41,3 @@ function mostrarVideo(index) {
   element.style.display = "flex";
   element.style.visibility = "visible";
 }
-// let foto = document.getElementById("slider_img_gemelas");
-// foto.addEventListener("click", mostrarVideoGemelas);
-// function mostrarVideoGemelas() {
-//   const iframe = document.getElementById("video_content__iframe");
-//   iframe.setAttribute("src", videoData.result[1].videoUrl);
-
-//   const textGemelas = videoData.result[1].text;
-
-//   const videoTextElement = document.getElementById("text_container__p");
-//   videoTextElement.innerText = textGemelas;
-//   const element = document.getElementById("video_content");
-//   element.style.display = "flex";
-//   element.style.visibility = "visible";
-// }
-
-// let fotoJack = document.getElementById("slider_img_jack");
-// fotoJack.addEventListener("click", mostrarVideoJack);
-// function mostrarVideoJack() {
-//   const iframe = document.getElementById("video_content__iframe");
-//   console.log(videoData.result);
-//   iframe.setAttribute("src", videoData.result[2].videoUrl);
-
-//   const textJack = videoData.result[2].text;
-
-//   const videoTextElement = document.getElementById("text_container__p");
-//   videoTextElement.innerText = textJack;
-//   const element = document.getElementById("video_content");
-//   element.style.display = "flex";
-//   element.style.visibility = "visible";
-// }
